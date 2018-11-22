a9c752e9d077fd04fb3cc87ad82f4a3dbcde149a
Craig Fletcher <43002140+Dovahstrun@users.noreply.github.com>
2018-11-22 16:11:18 +0000

    Changed the title to Sokoban

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ int main()
 
 	// Window - to draw to the screen
 	sf::RenderWindow gameWindow;
-	gameWindow.create(sf::VideoMode::getDesktopMode(), "Maze Escape", sf::Style::Titlebar | sf::Style::Close);
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Sokoban", sf::Style::Titlebar | sf::Style::Close);
 
 	// Seed our random number generator 
 	srand(time(NULL));
