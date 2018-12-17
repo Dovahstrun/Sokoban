f309c67cefdd55e1feef393e0ea830adc1536978
Dovahstrun <43002140+Dovahstrun@users.noreply.github.com>
2018-12-17 15:41:44 +0000

    Boxes are now storable

--- Box.cpp
+++ Box.cpp
@@ -1,14 +1,18 @@
 #include "Box.h"
 #include "Framework/Headers/AssetManager.h"
 #include "Level.h"
+#include "Storage.h"
 
 Box::Box()
 	: GridObject()
+	, m_stored(false)
 	, m_pushed()
+	, m_storeSound()
 {
 	m_sprite.setTexture(AssetManager::GetTexture("graphics/Box.png"));
 	m_blocksMovement = true;
 	m_pushed.setBuffer(AssetManager::GetSoundBuffer("audio/push.wav"));
+	m_storeSound.setBuffer(AssetManager::GetSoundBuffer("audio/stored.wav"));
 }
 
 bool Box::AttemptPush(sf::Vector2i _direction)
@@ -39,11 +43,51 @@ bool Box::AttemptPush(sf::Vector2i _direction)
 	if (!blocked)
 	{
 		m_pushed.play();
-		return m_level->MoveObjectTo(this, targetPos);
+		bool moveSucceeded =  m_level->MoveObjectTo(this, targetPos);
+
+		//If we DID successfully move
+		if (moveSucceeded)
+		{
+			//Check if we are stored
+			m_stored = false;
+
+			//Loop through the contents and see if a storage object was found
+			for (int i = 0; i < targetCellContents.size(); ++i)
+			{
+				Storage* storageObject = dynamic_cast<Storage*>(targetCellContents[i]);
+
+				//If dynamic cast succeeds, it will NOT be a nullptr (it is a storage object
+				if (storageObject != nullptr)
+				{
+					//The object is a storage object
+
+					//We are stored
+					m_stored = true;
+					m_storeSound.play();
+				}
+			}
+
+			//If we are, update our bool amd change our sprite
+			if (m_stored)
+			{
+				m_sprite.setTexture(AssetManager::GetTexture("graphics/boxStored.png"));
+			}
+			else
+			{
+				m_sprite.setTexture(AssetManager::GetTexture("graphics/Box.png"));
+			}
+		}
+
+		return moveSucceeded;
 	}
 
 	//If movement is blocked, do nothing, return false
 	//m_bump.play();
+	return false;
+}
+
+bool Box::getStored()
+{
 	return false;
 }
 
--- Box.h
+++ Box.h
@@ -12,9 +12,12 @@ public:
 	Box();
 
 	bool AttemptPush(sf::Vector2i _direction);
+	bool getStored();
 
 private:
 
+	bool m_stored;
 	sf::Sound m_pushed;
+	sf::Sound m_storeSound;
 
 };
